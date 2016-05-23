ca4bc70b0b113d5847652c64bb31a72446943ec6
Dnomyar96 <raymond.benjamins@telfort.nl>
2016-05-23 11:19:45 +0200

    Comments toegevoegd aan de SetTemperatureCommand klasse

--- projectHomeMatic/projectHomeMatic/SetTemperatureCommand.cs
+++ projectHomeMatic/projectHomeMatic/SetTemperatureCommand.cs
@@ -8,14 +8,22 @@ namespace projectHomeMatic
 {
     class SetTemperatureCommand
     {
-        private byte[] commandData = new byte[2];
+        private byte[] commandData = new byte[2]; //De data die via bluetooth verstuurd moet worden
 
+        /// <summary>
+        /// De array die via bluetooth wordt gestuurd wordt hier gevuld
+        /// </summary>
+        /// <param name="temperature"></param>
         public SetTemperatureCommand(double temperature)
         {
             commandData[0] = 65;
             commandData[1] = (byte)(int)(Convert.ToDouble(temperature) * 2.0D);
         }
 
+        /// <summary>
+        /// Methode die door andere classes kan worden aangeroepen om de commandData array op te halen
+        /// </summary>
+        /// <returns></returns>
         public byte[] GetCommandData()
         {
             return commandData;
